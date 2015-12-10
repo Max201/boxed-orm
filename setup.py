f5686b020a1dd850b527e0ce4890a88fad1be3a9
stassyn <alexskripkoit@gmail.com>
2015-12-10 15:00:15 +0200

    Boxed ORM Fixed pip library

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'rembox',
     packages = ['rembox'],
-    version = '0.1',
+    version = '0.11',
     description = 'Python easy database library',
     author = 'Maxim Papezhuk',
     author_email = 'maxp.job@gmail.com',
     url = 'https://github.com/Max201/boxed-orm',
-    download_url = 'https://github.com/Max201/boxed-orm/tarball/v0.1',
+    download_url = 'https://github.com/Max201/boxed-orm/tarball/v0.11',
     keywords = ['mysql', 'python', 'orm', 'easy'],
     classifiers = [],
 )
